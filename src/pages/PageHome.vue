6a442622c2466c5dae0cdb28dbf2cab883831907
Carlos Oporto <carlosoporto@gmail.com>
2019-06-18 15:48:22 -0500

    create NotFound Page

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -7,11 +7,9 @@
 
 <script>
 import sourceData from '@/data'
-import ThreadList from './ThreadList'
+import ThreadList from '@/components/ThreadList'
 console.log(sourceData)
 export default {
-  name: 'HelloWorld',
-
   components: {
     ThreadList
   },
---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-full">
+    <h1>Not Found</h1>
+    <p>
+      Oooops, we could not found what you are looking for. Why don't you
+      <router-link :to="{ name: 'Home' }">go home instead?</router-link>
+    </p>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+h1 {
+  font-size: 60px;
+}
+
+p {
+  font-size: 32px;
+  font-weight: 100;
+}
+
+div {
+  text-align: center;
+}
+</style>
